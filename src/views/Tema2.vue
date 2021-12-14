2ededaf01d35b9c1ba569d8ec08d01fa067da787
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-12-14 10:59:42 -0500

    Video

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -226,7 +226,7 @@
 
         figure.mb-5
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/E3YjXfabk40" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/UcfXPk7a98w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
             
 
 </template>
